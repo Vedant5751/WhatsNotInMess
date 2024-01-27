476305f4208f71979d86b2e7744e0edb44656f8a
Vedant Patil <vedant1346@gmail.com>
2024-01-27 16:46:52 +0530

    Navbar Updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="container mx-auto">
       <div className="flex justify-between items-center">
-        <h1 className="text-5xl font-bold my-8 mx-auto">What's not in mess</h1>
-        <div>
-          <Link to="/" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        <h1 className="text-3xl md:text-5xl font-bold my-4 md:my-8 mx-auto">
+          What's not in mess
+        </h1>
+        <div className="block md:hidden">
+          <button
+            onClick={toggleMenu}
+            className="text-blue-500 hover:text-blue-700 focus:outline-none focus:text-blue-700"
+          >
+            <svg
+              className="h-6 w-6 fill-current"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              {isOpen ? (
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M4 6h16v2H4V6zm0 5h16v2H4v-2zm16 5H4v2h16v-2z"
+                />
+              ) : (
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M4 6h16v2H4V6zm0 5h16v2H4v-2zm0 5h16v2H4v-2z"
+                />
+              )}
+            </svg>
+          </button>
+        </div>
+        <div className={`md:flex ${isOpen ? 'block' : 'hidden'}`}>
+          <Link
+            to="/"
+            className="block md:inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2 md:mt-0 md:mr-4"
+          >
             Home
           </Link>
-          <Link to="/detail" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          <Link
+            to="/detail"
+            className="block md:inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2 md:mt-0"
+          >
             Detail
           </Link>
         </div>
