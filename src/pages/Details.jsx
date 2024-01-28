9e6358ad90c86dd6175f230d02e57564a8fbd04f
piyushbaibhav <piyushbaibhav12@gmail.com>
2024-01-28 15:59:49 +0530

    detail text

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,16 +1,26 @@
 // Details.js
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React from 'react'
 
-function Details() {
-  const { id } = useParams();
-
+export default function Details() {
   return (
+    <>
+    <div>Details</div>
+
     <div>
-      <h2>Details for Card {id}</h2>
-      {/* Add additional details based on the card ID */}
+      Tired of using whatsInMess we've got you. WHATS-NOT-IN-MESS. now you dont need to ask your friend for phone no. for any delivery, you'll get everything here. menu , phone no., rating , location etc 
+
     </div>
-  );
+    <div>
+      Discover Local Dining Gems:
+    Our website is your one-stop destination for discovering the  eateries, and food joints in the vicinity of SRM College. if you've got cravings , we've got you covered.
+    </div>
+    <div>
+    Embark on a gastronomic journey around SRM College with SRM College Eats. Whether you're a student, faculty member, or visitor, our website is your ultimate companion for exploring the vibrant culinary scene near SRM College. Start discovering, dining, and indulging in unforgettable food experiences today!
+    </div>
+    
+
+ 
+    </>
+  )
 }
 
-export default Details;
